d458332792041486a08e58795937229c5b3f0acf
Diana <diana.barmina.2003@mail.ru>
2023-05-22 10:52:59 +0300

    add lab 5

--- students/K32421/DianaBarmina/LR_6/sql1.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('todo.db') # Warning: This file is created in the current directory
-conn.execute("CREATE TABLE readers (id INTEGER PRIMARY KEY, name char(50) NOT NULL, surname char(50) NOT NULL, birthday char(50) NOT NULL)")
-'''conn.execute("INSERT INTO readers (name, surname, birthday) VALUES ('Mike', 'Black', '04.05.2002')")
-conn.execute("INSERT INTO readers (name, surname, birthday) VALUES ('Sasha', 'White', '09.12.2001')")
-conn.execute("INSERT INTO readers (name, surname, birthday) VALUES ('Lena', 'Star', '11.10.2000')")
-conn.execute("DROP TABLE readers")'''
-conn.commit()
--- students/K32421/DianaBarmina/LR_6/sql2.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('todo.db') # Warning: This file is created in the current directory
-conn.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, name char(100) NOT NULL, author_name char(50) NOT NULL)")
-'''conn.execute("INSERT INTO books (name, author_name) VALUES ('Biology', 'Komarov V.V')")
-conn.execute("INSERT INTO books (name, author_name) VALUES ('Good Omens', 'Neil Geiman')")
-conn.execute("INSERT INTO books (name, author_name) VALUES ('Harry Potter', 'J.K.Rowling')")
-conn.execute("DROP TABLE books")'''
-conn.commit()
