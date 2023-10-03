fb757e26fed1663baa19ce45ff3804a552ccaf53
Владислав Пырков <pyrkovvlad@gmail.com>
2023-10-03 17:09:59 +0300

    Добавлены файлы и отчёты к лабораторным 2-4

---
+++ students/k32402/Pyrkov_Vladislav/LR4_sem2/bot.py
@@ -0,0 +1,141 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import TOKEN
+import sqlite3
+import database
+import random
+
+MSG = """
+Привет, {}! Добро пожаловать в Фильмобот! 💚
+
+Место, где ты можешь получить идеи, что посмотреть в свободное время
+Добавь свои первые фильмы!
+
+напиши /add_film , чтобы добавить фильм
+"""
+
+PHOTO = open('./img/film.jpg', 'rb')
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+
+def search_film(query):
+    conn = sqlite3.connect('films.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM films WHERE title LIKE ? OR director LIKE ? OR genre LIKE ?", 
+                   ('%{}%'.format(query), '%{}%'.format(query), '%{}%'.format(query)))
+    films = cursor.fetchall()
+    conn.close()
+    return films
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    user_full_name = message.from_user.full_name
+    user_id = message.from_user.id
+    database.add_user(user_id, user_full_name)
+    await bot.send_photo(user_id, PHOTO, MSG.format(user_full_name))
+
+@dp.message_handler(commands=['add_film'])
+async def add_film_handler(message: types.Message):
+    user_input = message.text[9:].strip()
+    if user_input:
+        film = database.find_film(user_input)
+        if film:
+            keyboard = InlineKeyboardMarkup()
+            keyboard.add(InlineKeyboardButton("Добавить", callback_data=f"add_{film[0]}"))
+            keyboard.add(InlineKeyboardButton("Отказаться", callback_data="cancel"))
+            await message.reply(f"Найден фильм: {film[1]} - {film[2]} ({film[3]})", reply_markup=keyboard)
+        else:
+            await message.reply("К сожалению, фильм не найден. Попробуйте еще раз!")
+    else:
+        await message.reply("Введите название фильма после команды /add_film")
+
+@dp.callback_query_handler(lambda call: call.data.startswith("add_"))
+async def add_film_callback_handler(call: types.CallbackQuery):
+    film_id = int(call.data[4:])
+    film = database.get_film_by_id(film_id)
+    if film:
+        database.add_film_to_user(call.from_user.id, film_id)
+        
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(InlineKeyboardButton("Показать мои фильмы", callback_data="show_films"))
+        keyboard.add(InlineKeyboardButton("Добавить еще фильм", callback_data="add_another"))
+
+        await call.message.reply(f"Фильм {film[1]} - {film[2]} ({film[3]}) добавлен!", reply_markup=keyboard)
+    else:
+        await call.answer(text="Произошла ошибка, попробуйте еще раз.")
+
+@dp.callback_query_handler(lambda call: call.data == "show_films")
+async def show_films_callback_handler(call: types.CallbackQuery):
+    films = database.get_user_films(call.from_user.id)
+    if films:
+        response = "Ваши фильмы:\n\n"
+        for film in films:
+            response += f"{film[1]} - {film[2]} ({film[3]})\n"
+        await call.message.reply(response)
+    else:
+        await call.message.reply("У вас пока нет добавленных фильмов.")
+
+@dp.callback_query_handler(lambda call: call.data == "add_another")
+async def add_another_callback_handler(call: types.CallbackQuery):
+    await call.message.reply("Введите название фильма после команды /add_film")
+
+
+@dp.message_handler(commands=['recommend'])
+async def recommend_handler(message: types.Message):
+    user_id = message.from_user.id
+    user_films = database.get_user_films(user_id)
+    if user_films:
+        recommended_films = []
+        for film in user_films:
+            genre_films = database.get_films_by_genre(user_id, film[3])
+            recommended_films.extend(genre_films)
+
+        if recommended_films:
+            film = random.choice(recommended_films)
+
+            keyboard = InlineKeyboardMarkup()
+            keyboard.add(InlineKeyboardButton("Добавить", callback_data=f"add_{film[0]}"))
+            keyboard.add(InlineKeyboardButton("Отказаться", callback_data="cancel"))
+
+            response = "Рекомендованный фильм:\n\n"
+            response += f"{film[1]} - {film[2]} ({film[3]})"
+            await message.reply(response, reply_markup=keyboard)
+        else:
+            await message.reply("К сожалению, нет новых рекомендаций по вашим жанрам. Попробуйте добавить больше фильмов!")
+    else:
+        await message.reply("Вы пока не добавили ни одного фильма. Добавьте хотя бы один, чтобы получить рекомендации.")
+
+@dp.message_handler(commands=['delete'])
+async def delete_handler(message: types.Message):
+    user_id = message.from_user.id
+    database.delete_user(user_id)
+    await message.reply("Ваша информация была удалена.")
+
+@dp.message_handler(commands=['delete_film'])
+async def delete_film_handler(message: types.Message):
+    user_input = message.text[12:].strip()
+    user_id = message.from_user.id
+    if user_input:
+        if database.check_film_added(user_id, user_input):
+            database.delete_film_from_user(user_id, user_input)
+            await message.reply(f"Фильм '{user_input}' была удалена из вашего списка.")
+        else:
+            await message.reply("Такого фильма у вас нет.")
+    else:
+        await message.reply("Введите название фильма после команды /delete_film")
+
+@dp.message_handler(commands=['show_films'])
+async def show_films_handler(message: types.Message):
+    user_id = message.from_user.id
+    films = database.get_user_films(user_id)
+    if films:
+        response = "Ваши фильмы:\n\n"
+        for film in films:
+            response += f"{film[1]} - {film[2]} ({film[3]})\n"
+        await message.reply(response)
+    else:
+        await message.reply("Вы ещё не добавили ни одного фильма.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
---
+++ students/k32402/Pyrkov_Vladislav/LR4_sem2/database.py
@@ -0,0 +1,161 @@
+import sqlite3
+
+def create_connection():
+    conn = sqlite3.connect('films.db')
+    return conn
+
+conn = create_connection()
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS films (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        director TEXT NOT NULL,
+        genre TEXT NOT NULL
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        full_name TEXT NOT NULL,
+        films_watched INTEGER NOT NULL
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user_films (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        film_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id),
+        FOREIGN KEY (film_id) REFERENCES films (id)
+    )
+''')
+
+films = [
+    ('Звездные войны', 'Джордж Лукас', 'фантастика, приключения'),
+    ('Властелин колец: Братство Кольца', 'Питер Джексон', 'фэнтези, приключения'),
+    ('Побег из Шоушенка', 'Фрэнк Дарабонт', 'драма, криминал'),
+    ('Форрест Гамп', 'Роберт Земекис', 'драма, комедия'),
+    ('В бой идут одни старики', 'Леонид Быков', 'военная драма'),
+    ('Зеленая миля', 'Фрэнк Дарабонт', 'драма, фэнтези'),
+    ('Игры разума', 'Рон Ховард', 'драма, биография'),
+    ('Темный рыцарь', 'Кристофер Нолан', 'боевик, триллер'),
+    ('Крестный отец', 'Фрэнсис Форд Коппола', 'криминал, драма'),
+    ('Бриллиантовая рука', 'Леонид Гайдай', 'комедия, криминал'),
+    ('Заводной апельсин', 'Стэнли Кубрик', 'драма, криминал, научная фантастика'),
+    ('Леон', 'Люк Бессон', 'боевик, триллер'),
+    ('Молчание ягнят', 'Джонатан Демми', 'триллер, детектив, криминал'),
+    ('Титаник', 'Джеймс Кэмерон', 'драма, романтика'),
+    ('Пианист', 'Роман Полански', 'драма, военная драма'),
+    ('Шоу Трумана', 'Питер Вир', 'комедия, драма'),
+    ('Исходный код', 'Дункан Джонс', 'фантастика, триллер'),
+    ('Пятый элемент', 'Люк Бессон', 'фантастика, приключения'),
+    ('Семь', 'Дэвид Финчер', 'триллер, детектив'),
+    ('Гладиатор', 'Ридли Скотт', 'боевик, драма'),
+    ('Список Шиндлера', 'Стивен Спилберг', 'драма, военная драма'),
+    ('Помни', 'Кристофер Нолан', 'драма, триллер, загадочный'),
+    ('Интерстеллар', 'Кристофер Нолан', 'фантастика, драма'),
+    ('Матрица', 'Лана и Лилли Вачовски', 'фантастика, боевик'),
+    ('В джазе только девушки', 'Билли Уайлдер', 'комедия, музыкальный'),
+    ('Поймай меня, если сможешь', 'Стивен Спилберг', 'криминал, биография'),
+    ('Американская история X', 'Тони Кэй', 'драма, криминал'),
+    ('Храброе сердце', 'Мел Гибсон', 'эпическая драма, военная драма'),
+    ('Город Бога', 'Катия Лунд', 'криминал, драма'),
+    ('Зеркало', 'Андрей Тарковский', 'драма, искусство'),
+]
+
+cursor.executemany('INSERT INTO films (title, director, genre) VALUES (?, ?, ?)', films)
+
+conn.commit()
+
+def find_film(query):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM films WHERE title LIKE ? OR director LIKE ?", (f"%{query}%", f"%{query}%"))
+    result = cursor.fetchone()
+    conn.close()
+    return result
+
+def add_user(user_id, full_name):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT OR IGNORE INTO users (id, full_name, films_watched) VALUES (?, ?, ?)", (user_id, full_name, 0))
+    conn.commit()
+    conn.close()
+
+def get_film_by_id(film_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM films WHERE id = ?", (film_id,))
+    result = cursor.fetchone()
+    conn.close()
+    return result
+
+def add_film_to_user(user_id, film_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO user_films (user_id, film_id) VALUES (?, ?)", (user_id, film_id))
+    conn.commit()
+    conn.close()
+
+def get_user_films(user_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT films.* FROM films
+        JOIN user_films ON user_films.film_id = films.id
+        WHERE user_films.user_id = ?
+    """, (user_id,))
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+def check_film_added(user_id, film_name):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT * FROM user_films
+        WHERE user_id = ? AND film_id = (SELECT id FROM films WHERE title = ?)
+    """, (user_id, film_name))
+    result = cursor.fetchone()
+    conn.close()
+    return result is not None
+
+def get_films_by_genre(user_id, genre):
+    conn = create_connection()
+    cursor = conn.cursor()
+    genres = genre.lower().split(', ')
+    films = []
+    for genre in genres:
+        cursor.execute("""
+            SELECT films.* FROM films
+            LEFT JOIN user_films ON user_films.film_id = films.id AND user_films.user_id = ?
+            WHERE LOWER(films.genre) LIKE ? AND user_films.film_id IS NULL
+        """, (user_id, f"%{genre}%"))
+        films.extend(cursor.fetchall())
+    conn.close()
+    return films
+
+def delete_user(user_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM user_films WHERE user_id = ?", (user_id,))
+    cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
+    conn.commit()
+    conn.close()
+
+def delete_film_from_user(user_id, film_name):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        DELETE FROM user_films 
+        WHERE user_id = ? AND film_id = (SELECT id FROM films WHERE title = ?)
+    """, (user_id, film_name))
+    conn.commit()
+    conn.close()
+
+
+conn.close()
