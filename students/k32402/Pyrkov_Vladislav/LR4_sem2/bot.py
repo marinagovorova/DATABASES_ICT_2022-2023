a9bb9020cac3496570273a290e57be53d5772661
Владислав Пырков <pyrkovvlad@gmail.com>
2023-10-04 13:33:35 +0300

    Добавлены исправления по лабораторным 2-4

--- students/k32402/Pyrkov_Vladislav/LR4_sem2/bot.py
+++ students/k32402/Pyrkov_Vladislav/LR4_sem2/bot.py
@@ -11,44 +11,61 @@ MSG = """
 Место, где ты можешь получить идеи, что посмотреть в свободное время
 Добавь свои первые фильмы!
 
-напиши /add_film , чтобы добавить фильм
+Доступные команды:
+/add_film - добавить фильм, принимает: название фильма
+/recommend - получить рекомендацию
+/show_films - показать ваши фильмы
+/update_director - обновить режиссёра фильма, принимает: название фильма, новое имя режиссёра
+/delete - удалить всю вашу информацию
+/delete_film - удалить фильм из вашего списка
 """
 
-PHOTO = open('./img/film.jpg', 'rb')
+PHOTO = open("./img/film.jpg", "rb")
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
+
 def search_film(query):
-    conn = sqlite3.connect('films.db')
+    conn = sqlite3.connect("films.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM films WHERE title LIKE ? OR director LIKE ? OR genre LIKE ?", 
-                   ('%{}%'.format(query), '%{}%'.format(query), '%{}%'.format(query)))
+    cursor.execute(
+        "SELECT * FROM films WHERE title LIKE ? OR director LIKE ? OR genre LIKE ?",
+        ("%{}%".format(query), "%{}%".format(query), "%{}%".format(query)),
+    )
     films = cursor.fetchall()
     conn.close()
     return films
 
-@dp.message_handler(commands=['start'])
+
+@dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
     user_full_name = message.from_user.full_name
     user_id = message.from_user.id
     database.add_user(user_id, user_full_name)
     await bot.send_photo(user_id, PHOTO, MSG.format(user_full_name))
 
-@dp.message_handler(commands=['add_film'])
+
+@dp.message_handler(commands=["add_film"])
 async def add_film_handler(message: types.Message):
     user_input = message.text[9:].strip()
     if user_input:
         film = database.find_film(user_input)
         if film:
             keyboard = InlineKeyboardMarkup()
-            keyboard.add(InlineKeyboardButton("Добавить", callback_data=f"add_{film[0]}"))
+            keyboard.add(
+                InlineKeyboardButton("Добавить", callback_data=f"add_{film[0]}")
+            )
             keyboard.add(InlineKeyboardButton("Отказаться", callback_data="cancel"))
-            await message.reply(f"Найден фильм: {film[1]} - {film[2]} ({film[3]})", reply_markup=keyboard)
+            await message.reply(
+                f"Найден фильм: {film[1]} - {film[2]} ({film[3]})",
+                reply_markup=keyboard,
+            )
         else:
             await message.reply("К сожалению, фильм не найден. Попробуйте еще раз!")
     else:
         await message.reply("Введите название фильма после команды /add_film")
+
 
 @dp.callback_query_handler(lambda call: call.data.startswith("add_"))
 async def add_film_callback_handler(call: types.CallbackQuery):
@@ -56,14 +73,21 @@ async def add_film_callback_handler(call: types.CallbackQuery):
     film = database.get_film_by_id(film_id)
     if film:
         database.add_film_to_user(call.from_user.id, film_id)
-        
-        keyboard = InlineKeyboardMarkup()
-        keyboard.add(InlineKeyboardButton("Показать мои фильмы", callback_data="show_films"))
-        keyboard.add(InlineKeyboardButton("Добавить еще фильм", callback_data="add_another"))
 
-        await call.message.reply(f"Фильм {film[1]} - {film[2]} ({film[3]}) добавлен!", reply_markup=keyboard)
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(
+            InlineKeyboardButton("Показать мои фильмы", callback_data="show_films")
+        )
+        keyboard.add(
+            InlineKeyboardButton("Добавить еще фильм", callback_data="add_another")
+        )
+
+        await call.message.reply(
+            f"Фильм {film[1]} - {film[2]} ({film[3]}) добавлен!", reply_markup=keyboard
+        )
     else:
         await call.answer(text="Произошла ошибка, попробуйте еще раз.")
+
 
 @dp.callback_query_handler(lambda call: call.data == "show_films")
 async def show_films_callback_handler(call: types.CallbackQuery):
@@ -76,12 +100,13 @@ async def show_films_callback_handler(call: types.CallbackQuery):
     else:
         await call.message.reply("У вас пока нет добавленных фильмов.")
 
+
 @dp.callback_query_handler(lambda call: call.data == "add_another")
 async def add_another_callback_handler(call: types.CallbackQuery):
     await call.message.reply("Введите название фильма после команды /add_film")
 
 
-@dp.message_handler(commands=['recommend'])
+@dp.message_handler(commands=["recommend"])
 async def recommend_handler(message: types.Message):
     user_id = message.from_user.id
     user_films = database.get_user_films(user_id)
@@ -95,24 +120,32 @@ async def recommend_handler(message: types.Message):
             film = random.choice(recommended_films)
 
             keyboard = InlineKeyboardMarkup()
-            keyboard.add(InlineKeyboardButton("Добавить", callback_data=f"add_{film[0]}"))
+            keyboard.add(
+                InlineKeyboardButton("Добавить", callback_data=f"add_{film[0]}")
+            )
             keyboard.add(InlineKeyboardButton("Отказаться", callback_data="cancel"))
 
             response = "Рекомендованный фильм:\n\n"
             response += f"{film[1]} - {film[2]} ({film[3]})"
             await message.reply(response, reply_markup=keyboard)
         else:
-            await message.reply("К сожалению, нет новых рекомендаций по вашим жанрам. Попробуйте добавить больше фильмов!")
+            await message.reply(
+                "К сожалению, нет новых рекомендаций по вашим жанрам. Попробуйте добавить больше фильмов!"
+            )
     else:
-        await message.reply("Вы пока не добавили ни одного фильма. Добавьте хотя бы один, чтобы получить рекомендации.")
+        await message.reply(
+            "Вы пока не добавили ни одного фильма. Добавьте хотя бы один, чтобы получить рекомендации."
+        )
 
-@dp.message_handler(commands=['delete'])
+
+@dp.message_handler(commands=["delete"])
 async def delete_handler(message: types.Message):
     user_id = message.from_user.id
     database.delete_user(user_id)
     await message.reply("Ваша информация была удалена.")
 
-@dp.message_handler(commands=['delete_film'])
+
+@dp.message_handler(commands=["delete_film"])
 async def delete_film_handler(message: types.Message):
     user_input = message.text[12:].strip()
     user_id = message.from_user.id
@@ -125,7 +158,8 @@ async def delete_film_handler(message: types.Message):
     else:
         await message.reply("Введите название фильма после команды /delete_film")
 
-@dp.message_handler(commands=['show_films'])
+
+@dp.message_handler(commands=["show_films"])
 async def show_films_handler(message: types.Message):
     user_id = message.from_user.id
     films = database.get_user_films(user_id)
@@ -137,5 +171,23 @@ async def show_films_handler(message: types.Message):
     else:
         await message.reply("Вы ещё не добавили ни одного фильма.")
 
-if __name__ == '__main__':
+
+@dp.message_handler(commands=["update_director"])
+async def update_director_handler(message: types.Message):
+    user_input = message.text[16:]
+    print(user_input)
+    if user_input:
+        film = database.find_film(user_input.split()[0])
+        if film:
+            database.update_film_director(film[1], user_input.split()[1])
+            await message.reply(
+                "Теперь у фильма "
+                + film[1]
+                + " режиссёр "
+                + user_input.split()[1]
+                + " !"
+            )
+
+
+if __name__ == "__main__":
     executor.start_polling(dp)
